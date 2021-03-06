094a105366faca8680dfface62cd0d3861e755be
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-03-05 20:47:29 -0500

    create map

---
+++ Leaflet-Step 1/static/js/logic.js
@@ -0,0 +1,123 @@
+
+  var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+    function createMap(earthquakes) {
+
+        // Define streetmap and darkmap layers
+        var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+          tileSize: 512,
+          maxZoom: 3,
+          zoomOffset: -1,
+          id: "mapbox/streets-v11",
+          accessToken: API_KEY
+        });
+      
+        var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+          maxZoom: 3,
+          id: "dark-v10",
+          accessToken: API_KEY
+        });
+      
+        // Define a baseMaps object to hold our base layers
+        var baseMaps = {
+          "Street Map": streetmap,
+          "Dark Map": darkmap
+        };
+      
+        // Create overlay object to hold our overlay layer
+        var overlayMaps = {
+          Earthquakes: earthquakes
+        };
+      
+        // Create our map, giving it the streetmap and earthquakes layers to display on load
+        var myMap = L.map("map", {
+          center: [
+            37.09, -95.71
+          ],
+          zoom: 5,
+          layers: [streetmap, earthquakes]
+        });
+      
+        // Create a layer control
+        // Pass in our baseMaps and overlayMaps
+        // Add the layer control to the map
+        L.control.layers(baseMaps, overlayMaps, {
+          collapsed: false
+        }).addTo(myMap);
+    }
+    function createFeatures(earthquakeData) {
+  
+        // Define a function we want to run once for each feature in the features array
+        // Give each feature a popup describing the place and time of the earthquake
+        function onEachFeature(feature, layer) {
+          layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+        }
+        var earthquakes = L.geoJSON(earthquakeData, {
+            onEachFeature: onEachFeature
+            // pointtoLayer: function(earthquakeData, latlng){
+            //     return L.circleMarker(latlng, quakeMarker);
+            // } 
+          });
+        // var quakeMarker = {
+        //     stroke: false,
+        //     fillOpacity: 0.75,
+        //     color: "white",
+        //     fillColor: "blue",
+        //     radius: 38
+            // radius: markerSize(locations[i].city.population)
+              
+        //   };
+          
+          // Sending our earthquakes layer to the createMap function
+          createMap(earthquakes);
+    }
+    
+    // Perform a GET request to the query URL
+    d3.json(queryUrl, function(data) {
+        // Once we get a response, send the data.features object to the createFeatures function
+        createFeatures(data.features);
+        console.log(data.features);
+    });
+
+for (var i = 0; i < countries.length; i++) {
+
+    function colorSel(depth){
+          var color = "";
+          if (depth <10) {
+            color = "#ccff66";
+          }
+          else if (depth<30) {
+            color = "#ffff66";
+          }
+          else if (depth < 50) {
+            color = "ffcc66";
+          }
+          else if (depth < 50) {
+            color = "#ffa64d";
+          }
+          else if (depth < 50) {
+            color = "#e67300";
+          }
+          else {
+            color = "#ff4d4d";
+          }
+        return color
+        }
+        
+        color = colorSel(feature[i].geometry.coordinates[2]);
+    
+    function markerSize(magnitude) {
+         return magnitude *100;
+    }
+
+    L.circle(locations[i].coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: color,
+      radius: markerSize(locations[i].city.population)
+    })
+}
+
