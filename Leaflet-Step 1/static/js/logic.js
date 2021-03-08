a5419b913eddea8ce67eb1b52f249aa1866d7ea3
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-03-08 18:52:19 -0500

    legend

--- Leaflet-Step 1/static/js/logic.js
+++ Leaflet-Step 1/static/js/logic.js
@@ -51,6 +51,35 @@
         L.control.layers(baseMaps, overlayMaps, {
           collapsed: false
         }).addTo(myMap);
+                // legend creation
+    function getColor(d) {
+      return d > 90 ? '#ff4d4d' :
+             d > 70  ? '#e67300' :
+             d > 50  ? '#ffa64d' :
+             d > 30  ? '#ffcc66' :
+             d > 10  ? '#ffff66"' :
+                        '#c4ff4d';
+          
+    }
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function () {
+    
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 50, 70, 90],
+            labels = [];
+    
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+    
+        return div;
+    };
+    
+    legend.addTo(myMap);
     }
     function createFeatures(earthquakeData) {
   
@@ -75,7 +104,7 @@
           }
         }
      
-      
+        
       function markerSize(magnitude) {
               return magnitude *3;
       }
@@ -104,9 +133,13 @@
         );
       }
     });
+
     // .addTo(map);
-   
+    
+    
           // Sending our earthquakes layer to the createMap function
+          
           createMap(earthquakes);
+  
     };
     
